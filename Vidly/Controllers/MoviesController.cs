56629555263c9b583b2d1d700e511958b76b2c6b
goran7t <goran7t@yahoo.com>
2017-12-21 11:15:11 +0100

    Exercise 2

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -10,6 +8,30 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        // GET: movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies() => new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Toy Story"}
+            };
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -28,27 +50,11 @@ namespace Vidly.Controllers
             };
 
             return View(viewModel);
-            //return Content("Hello world!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
 
         public ActionResult Edit(int id)
         {
             return Content("id = " + id);
-        }
-
-        // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex = {0} & sortBy = {1}", pageIndex, sortBy));
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
