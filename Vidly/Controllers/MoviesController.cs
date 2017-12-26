103342683ba273d15947f58bbc85d546782bf9d3
goran7t <goran7t@yahoo.com>
2017-12-26 10:35:19 +0100

    Section 3 Exercise

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -8,22 +9,34 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: movies
         public ViewResult Index()
         {
-            var movies = GetMovies();
-
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+            
             return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movie = GetMovies().SingleOrDefault(c => c.Id == id);
-
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            
             if (movie == null)
                 return HttpNotFound();
 
-            return View(movie);
+            return View(movie);            
         }
 
         private IEnumerable<Movie> GetMovies() => new List<Movie>
