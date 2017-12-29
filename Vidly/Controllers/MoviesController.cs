3db767ddfd04d9b8ddef716f99c47fc527aa2e1f
goran7t <goran7t@yahoo.com>
2017-12-29 16:37:16 +0100

    Section 4 - Finished Exercise

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -47,7 +47,40 @@ namespace Vidly.Controllers
                 Genres = genres
             };
 
-            return View(viewModel);
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");            
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm", viewModel);
         }
 
         private IEnumerable<Movie> GetMovies() => new List<Movie>
@@ -74,11 +107,6 @@ namespace Vidly.Controllers
             };
 
             return View(viewModel);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id = " + id);
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
